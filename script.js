14ce4d7da0077290bed4be413a3b614756644f3a
Yulong Fan <91830736+Dlong12@users.noreply.github.com>
2026-02-25 11:26:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -66,6 +66,56 @@ async function loadFAOD() {
   }
 
   console.log('fAOD overlays added:', added);
+  /**
+ * 根据类型字段和值确定点颜色
+ */
+function getColor(field, val) {
+    let colors = ["rgb(49, 54, 149)",
+        "rgb(74, 123, 183)",
+        "rgb(128, 183, 214)",
+        "rgb(189, 226, 238)",
+        "rgb(238, 248, 223)",
+        "rgb(254, 238, 165)",
+        "rgb(253, 191, 113)",
+        "rgb(246, 123, 74)",
+        "rgb(218, 55, 42)",
+        "rgb(165, 0, 38)"];
+    if (field == "fAOD") {
+        if (val == 0) {
+            return colors[0];
+        } else if (val <= 0.125) {
+            return colors[1];
+        } else if (val <= 0.25) {
+            return colors[2];
+        } else if (val <= 0.375) {
+            return colors[3];
+        } else if (val <= 0.5) {
+            return colors[4];
+        } else if (val <= 0.625) {
+            return colors[5];
+        } else if (val <= 0.75) {
+            return colors[6];
+        } else if (val <= 0.875) {
+            return colors[7];
+        } else {
+            return colors[8];
+        }
+    }
+  /*
+ *根据气体类型更新图例   
+*/
+function updateLegendLabel() {
+    if (selectType == "type-fAOD") {
+        document.getElementById('gas-label').innerHTML = 'fAOD:';
+        document.getElementById('tick0').innerHTML = '0.125';
+        document.getElementById('tick1').innerHTML = '0.25';
+        document.getElementById('tick2').innerHTML = '0.375';
+        document.getElementById('tick3').innerHTML = '0.5';
+        document.getElementById('tick4').innerHTML = '0.625';
+        document.getElementById('tick5').innerHTML = '0.75';
+        document.getElementById('tick6').innerHTML = '0.875';
+    }
+  
 
   // 3) 视图拉到覆盖范围，便于确认是否显示
   map.fitBounds(IMAGE_BOUNDS);
